f1d0938263ebfb6d8235ef89fe36c62d72a27f65
luming hao <lh00000000@gmail.com>
2020-10-09 13:06:39 -0400

    fix: add sendBefore arg to sendMessage (#19)
    Reviewed-by: @bchrobot

--- src/lib/sms/queries.ts
+++ src/lib/sms/queries.ts
@@ -48,8 +48,8 @@ export const DELETE_SENDING_LOCATION = `
 `;
 
 export const SEND_MESSAGE = `
-  mutation sendMessage($profileId: UUID!, $to: PhoneNumber!, $body: String!, $contactZipCode: ZipCode, $mediaUrls: [Url]) {
-    sendMessage(input: {profileId: $profileId, to: $to, body: $body, contactZipCode: $contactZipCode, mediaUrls: $mediaUrls}) {
+  mutation sendMessage($profileId: UUID!, $to: PhoneNumber!, $body: String!, $contactZipCode: ZipCode, $mediaUrls: [Url], $sendBefore: String!) {
+    sendMessage(input: {profileId: $profileId, to: $to, body: $body, contactZipCode: $contactZipCode, mediaUrls: $mediaUrls, sendBefore: $sendBefore}) {
       outboundMessage {
         id
       }
